33ae55913f37b424e1e99a10b4a07655fb5af368
piseth04 <piseth3025@gmail.com>
2025-03-01 14:52:55 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import Button from 'antd/es/button';
+// import Button from 'antd/es/button';
 import Card from 'antd/es/card';
 import React from 'react';
 
 const App: React.FC = () => {
-  const handleClick = () => {
-    alert('Clicked!');
-  };
+  // const handleClick = () => {
+  //   alert('Clicked!');
+  // };
 
   return (
+    
     <div style={{ padding: '20px' }}>
-      <h1>Welcome to React with Ant Design</h1>
-      <Button type="primary" onClick={handleClick}>
+      {/* <Button type="primary" onClick={handleClick}>
         Click Me
-      </Button>
-      <Card title="Sample Card" style={{ marginTop: '20px', maxWidth: '300px' }}>
-        <p>This is a sample card using Ant Design.</p>
+      </Button> */}
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome to React with Ant Design</h1>
+      </div>
+      
+      <Card title="Sample Card" style={{ marginTop: '20px', textAlign: 'center', maxWidth: '3000px' }}>
+        <div style={{ textAlign: 'left' }}> 
+          <p>This is a sample card using Ant Design.</p>
+        </div>
       </Card>
     </div>
   );
